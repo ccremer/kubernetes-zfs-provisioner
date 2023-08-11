887c037e172b5a5cc140ca3aeab961a92d9b7082
Dirk Kok <the@dirkkok.nl>
2023-08-11 16:22:47 +0200

    fix template issue and add chart unit test

--- charts/kubernetes-zfs-provisioner/test/storageclass_test.go
+++ charts/kubernetes-zfs-provisioner/test/storageclass_test.go
@@ -59,6 +59,51 @@ func Test_StorageClass_GivenClassesEnabled_WhenNodeDefined_ThenRenderNodeName(t 
 	assert.Equal(t, "hostpath", class.Parameters["type"])
 }
 
+func Test_StorageClass_GivenClassesEnabled_WhenReserveSpaceUndefined_ThenRenderDefault(t *testing.T) {
+	options := &helm.Options{
+		ValuesFiles: []string{"values/storageclass_1.yaml"},
+	}
+
+	output := helm.RenderTemplate(t, options, helmChartPath, releaseName, tplStorageclass)
+
+	var class v1.StorageClass
+	helm.UnmarshalK8SYaml(t, output, &class)
+
+	assert.Equal(t, "true", class.Parameters["reserveSpace"])
+}
+
+func Test_StorageClass_GivenClassesEnabled_WhenReserveSpaceFalse_ThenRenderReserveSpace(t *testing.T) {
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"storageClass.create":                  "true",
+			"storageClass.classes[0].reserveSpace": "false",
+		},
+	}
+
+	output := helm.RenderTemplate(t, options, helmChartPath, releaseName, tplStorageclass)
+
+	var class v1.StorageClass
+	helm.UnmarshalK8SYaml(t, output, &class)
+
+	assert.Equal(t, "false", class.Parameters["reserveSpace"])
+}
+
+func Test_StorageClass_GivenClassesEnabled_WhenReserveSpaceTrue_ThenRenderReserveSpace(t *testing.T) {
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"storageClass.create":                  "true",
+			"storageClass.classes[0].reserveSpace": "true",
+		},
+	}
+
+	output := helm.RenderTemplate(t, options, helmChartPath, releaseName, tplStorageclass)
+
+	var class v1.StorageClass
+	helm.UnmarshalK8SYaml(t, output, &class)
+
+	assert.Equal(t, "true", class.Parameters["reserveSpace"])
+}
+
 func Test_StorageClass_GivenClassesEnabled_WhenAdditionalParametersUndefined_ThenRenderEmptyValues(t *testing.T) {
 	options := &helm.Options{
 		ValuesFiles: []string{"values/storageclass_1.yaml"},
