9bc2c080a052c436911deb0436f7c259fa11ad74
Andre Klärner <kandre@ak-online.be>
2026-08-18 23:07:29 +0200

    add an empty test/provision_integration.go package
    This only exists so renovate will automatically upgrade major version
    import paths

---
+++ test/provision_integration.go
@@ -0,0 +1,4 @@
+// this empty package exists only, so the provision_integration_test.go
+// (which is only build under build tag integration) is found by
+// golang.org/x/tools/go/packages, which is use by github.com/marwan-at-work/mod.
+package test
